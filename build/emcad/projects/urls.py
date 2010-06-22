ef4638931ebbf080cb61e8d4767315e85fe01f03
Taylan Pince <taylanpince@gmail.com>
2010-06-21 20:13:04 -0400

    Finalized dev environment build

--- build/emcad/projects/urls.py
+++ build/emcad/projects/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 
-urlpatterns += patterns('projects.views',
+urlpatterns = patterns('projects.views',
     url(r"^$", "categories_list", name="projects_categories"),
     url(r"^(?P<category_slug>[-\w]+)/$", "category_detail", name="projects_category"),
     url(r"^(?P<category_slug>[-\w]+)/(?P<project_slug>[-\w]+)/$", "project_detail", name="projects_project"),
